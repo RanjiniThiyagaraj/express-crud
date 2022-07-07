efa4d9423c2d24b630fed427ad4f8051a9c88724
sivaranjini <sivaranjinithiyagaraj@gmail.com>
2022-07-07 10:54:41 +0400

    prod

--- routes/product.js
+++ routes/product.js
@@ -6,4 +6,7 @@ const productController = require("../app/api/controller/product");
 router.post("/create", jwt.validateToken, productController.create);
 router.get("/", productController.getAll);
 router.get("/:id", productController.getById);
+router.delete("/:id", productController.deleteById);
+router.patch("/:id",productController.updateById);
+
 module.exports = router;
